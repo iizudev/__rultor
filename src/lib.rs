f2d8efab41204358f42b36b84cce586615d4668e
iizudev <izudev@yandex.ru>
2025-03-25 14:42:42 +0500

    polish

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 //! SPDX-License-Identifier: MIT OR Apache-2.0
-//!
-//! [`bevy_actify`](https://crates.io/crates/bevy_actify)'s derive macros.
+#![doc = include_str!("../README.md")]
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
 
+/// Derive macro implementation for `InputAction` trait
 #[proc_macro_derive(InputAction)]
 pub fn input_action_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -13,7 +14,8 @@ pub fn input_action_derive(input: TokenStream) -> TokenStream {
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
     let expanded = quote! {
-        impl #impl_generics InputAction for #name #ty_generics #where_clause {}
+        #[automatically_derived]
+        impl #impl_generics ::bevy_actify::InputAction for #name #ty_generics #where_clause {}
     };
 
     TokenStream::from(expanded)
